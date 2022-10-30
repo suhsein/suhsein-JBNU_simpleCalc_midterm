08128222caa8e483e56397f915e7c32fe32f7691
suhsein <mini9075@naver.com>
2022-10-31 00:41:21 +0900

    Revert "code structure improvement"
    This reverts commit e1d4801f14fddd4b72f2779c0901d494b6b1d87a.

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 # Program make a simple calculator
-import calculator as cc
-import logging_config as lg
-import term_loop as tl
+
+#logging 설정
+import logging
+
+Log_Format = "%(levelname)s %(asctime)s     %(message)s"
+
+logging.basicConfig(filename = "logfile.log",
+                    filemode = "w",
+                    format = Log_Format, 
+                    level = logging.INFO)
+
+logger = logging.getLogger()
+
+# Program make a simple calculator
+
+op = ['','+', '-', '*', '/']
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+#Need to define divide function.
+def divide (x,y):
+    return x/y
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
-print("4.Divide")   
+print("4.Divide") 
+
 
 while True:
     # take input from the user
@@ -19,38 +50,62 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            str = "%.1f + %.1f = %.1f" % (num1, num2, cc.add(num1,num2))
+            str = "%d + %d = %d" % (num1, num2, add(num1,num2))
             print(str)
-            lg.logging.info(str)
+            logging.info(str)
 
         elif choice == '2':
-            str = "%.1f - %.1f = %.1f" % (num1, num2, cc.subtract(num1,num2))
+            str = "%d - %d = %d" % (num1, num2, subtract(num1,num2))
             print(str)
-            lg.logging.info(str)
+            logging.info(str)
 
         elif choice == '3':
-            str = "%.1f * %.1f = %.1f" % (num1, num2, cc.multiply(num1,num2))
+            str = "%d * %d = %d" % (num1, num2, multiply(num1,num2))
             print(str)
-            lg.logging.info(str)
+            logging.info(str)
 
         elif choice =='4':
             if(num2 == 0):
-              lg.logging.warning("Division by Zero")
+              logging.warning("Division by Zero")
               print("You can't divide by zero.")
             else:
-              str = "%.1f / %.1f = %.1f" % (num1, num2, cc.divide(num1,num2))
+              str = "%d / %d = %d" % (num1, num2, divide(num1,num2))
               print(str)
-              lg.logging.info(str)
+              logging.info(str)
 
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
         flag = False
-        flag = tl.term(flag)
+
+        while True:
+          next_calculation = input("Let's do next calculation? (yes/no): ")
+          next_calculation = next_calculation.lower()
+
+          if next_calculation == "yes":
+              flag = True
+              break
+          elif next_calculation == "no":
+              confirm = input("Are you sure? (yes/no): ")
+              confirm = confirm.lower()
+              
+              if confirm == "yes":
+                break
+              elif confirm == "no":
+                continue
+              else:
+                print("Invalid Input. Please answer yes or no.")
+                #로깅?
+                continue
+          else:
+            print("Invalid Input. Please answer yes or no.")
+            #로깅?
+            continue
+        
         if flag == False:
           break
-        else:
-          continue
-
 
     else:
-        lg.logging.warning("Invalid Input")
+        logging.warning("Invalid Input")
         print("Invalid Input")
         
