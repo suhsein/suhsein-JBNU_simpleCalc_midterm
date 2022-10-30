9013e45590b30609419d9fd8238aa40bd157a6a7
suhsein <mini9075@naver.com>
2022-10-30 22:27:38 +0900

    Input improvement
    Forced input text to lowercase.
    Dealt with invalid input.(not yes or no)

--- main.py
+++ main.py
@@ -50,18 +50,17 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            # print(num1, "+", num2, "=", add(num1, num2))
-            str = "%d %s %d = %d" % (num1, op[1], num2, add(num1,num2))
+            str = "%d + %d = %d" % (num1, num2, add(num1,num2))
             print(str)
             logging.info(str)
 
         elif choice == '2':
-            str = "%d %s %d = %d" % (num1, op[2], num2, subtract(num1,num2))
+            str = "%d - %d = %d" % (num1, num2, subtract(num1,num2))
             print(str)
             logging.info(str)
 
         elif choice == '3':
-            str = "%d %s %d = %d" % (num1, op[3], num2, multiply(num1,num2))
+            str = "%d * %d = %d" % (num1, num2, multiply(num1,num2))
             print(str)
             logging.info(str)
 
@@ -69,16 +68,32 @@ while True:
             if(num2 == 0):
               logging.warning("Division by Zero")
             else:
-              str = "%d %s %d = %d" % (num1, op[4], num2, divide(num1,num2))
+              str = "%d / %d = %d" % (num1, num2, divide(num1,num2))
               print(str)
               logging.info(str)
 
 
         # check if user wants another calculation
         # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
+        flag = False
+
+        while True:
+          next_calculation = input("Let's do next calculation? (yes/no): ")
+          next_calculation = next_calculation.lower()
+
+          if next_calculation == "yes":
+              flag = True
+              break
+          elif next_calculation == "no":
+              flag = False
+              break
+          else:
+            print("Invalid Input. Please answer yes or no.")
+            #로깅?
+            continue
+        
+        if flag == False:
+          break
 
     else:
         logging.warning("Invalid Input")
